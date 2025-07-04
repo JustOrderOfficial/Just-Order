2d752f2f1641a5362fc5f4960f1640ae3a05a517
JustOrderOfficial <justorderofficial@gmail.com>
2025-07-04 16:49:02 +0530

    Added the necessary changes for sign up and sign in with google

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId="932769350992-g6oekq9ogs8uldu9a5bgbnd2v4tunklo.apps.googleusercontent.com">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>,
-)
+);
